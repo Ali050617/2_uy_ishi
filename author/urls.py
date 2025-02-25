8cdc736fd356b80d06e8d0a5789823f177756964
Muhammadali <muhammadalikadyrov06@gmail.com>
2025-02-25 14:34:29 +0500

    hatoliklar halo bor

--- author/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('authors/', views.AuthorListCreateView.as_view(), name='author_list'),
-    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author_detail'),
-]
